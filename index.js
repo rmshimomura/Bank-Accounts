bf005931ebe25bfbe02077cc98651456035995cc
rmshimomura <shimomurarodrigo1@gmail.com>
2022-01-09 23:47:35 -0300

    Creating account

--- index.js
+++ index.js
@@ -27,9 +27,59 @@ function operation () {
     .then((answer) =>{ 
 
         const action = answer['action'];
-        
+
+        switch(action) {
+            case 'Create account':
+                createAccount()
+            break
+            
+        }
 
     })
     .catch(err => {console.error(err)})
+
+}
+
+function createAccount() {
+
+    console.log(chalk.bgGreen.yellow('Congratulations for using our bank!'))
+    console.log(chalk.green('Please choose the account options:'))
+
+    buildAccount()
+
+}
+
+function buildAccount() {
+
+    inquirer.prompt([
+        {
+            name: "accountName",
+            message: "Enter your name:",
+        },
+        
+    ])
+    .then((answers) => {
+
+        const accountName = answers["accountName"]
+        
+        if(!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+
+            console.log(chalk.bgRed.white("Account already exists. Please try again"))
+            buildAccount()
+            return
+
+        } else {
+
+            fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', err => { console.log(err) })
+            console.log(chalk.green("Account created successfully."))
+            operation()
+
+        }
+
+    })
 
 }
