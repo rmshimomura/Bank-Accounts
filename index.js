0f8ae330bdc79fd3d8ed78353b2b33bdec31a7dc
rmshimomura <shimomurarodrigo1@gmail.com>
2022-01-09 23:01:00 -0300

    Showing question on terminal prompt

--- index.js
+++ index.js
@@ -6,4 +6,30 @@ const chalk = require('chalk')
 // Intern Modules
 const fs = require('fs')
 
-console.log("Starting Accounts project")
+operation()
+
+function operation () {
+
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'What actions do you want to make?',
+        choices : [
+            'Create account',
+            'Check balance',
+            'Deposit',
+            'Withdraw',
+            'Exit'
+        ]
+    },
+
+    ])
+    .then((answer) =>{ 
+
+        const action = answer['action'];
+        
+
+    })
+    .catch(err => {console.error(err)})
+
+}
