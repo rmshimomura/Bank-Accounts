d942caae032fb286ac09b0f2dddba919dccf3c60
rmshimomura <shimomurarodrigo1@gmail.com>
2022-01-11 12:10:36 -0300

    Transference function implemented

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ function operation() {
             'Check balance',
             'Deposit',
             'Withdraw',
+            'Transfer between accounts',
+            'Check interest rate',
             'Exit'
         ]
     },
@@ -46,10 +48,18 @@ function operation() {
                     withdraw()
                     break
 
+                case 'Transfer between accounts':
+                    transfer()
+                    break
+
+                case 'Check interest rate':
+                    interestRate()
+                    break
+
                 case 'Exit':
                     console.log(chalk.bgBlue.white('Thanks for using accounts service!'));
                     process.exit();
-                    break
+                    
             }
 
         })
@@ -277,7 +287,7 @@ function removeAmount(accountName, amount) {
 
     const account = getAccount(accountName)
 
-    if(!amount) {
+    if(!amount || amount < 0) {
 
         console.log(chalk.bgRed.black('An error occurred please try again later.'))
         return withdraw()
@@ -298,5 +308,96 @@ function removeAmount(accountName, amount) {
     console.log(chalk.green.bold(`Withdraw of U$${amount} successfully made on account ${accountName}`))
 
     operation()
+
+}
+
+function transfer() { 
+    
+    inquirer.prompt([
+
+        {
+            name: 'accountFrom',
+            message: 'Please type the first account name',
+        },
+        {
+
+            name: 'accountTo',
+            message: 'Please type the second account name',
+
+        }
+
+    ])
+        .then(answers => {
+
+            const accountFrom = answers['accountFrom'];
+            const accountTo = answers['accountTo'];
+
+
+            if(!checkAccount(accountFrom)) {
+
+                console.log(chalk.bgRed.yellowBright("An error occurred, please try again later (first account not found)"))
+                
+                return transfer();
+
+            } else if(!checkAccount(accountTo)) {
+
+                console.log(chalk.bgRed.yellowBright("An error occurred, please try again later (second account not found)"))
+
+                return transfer();
+
+            } else {
+
+                inquirer.prompt([
+
+                    {
+                        name: 'transferenceValue',
+                        message: 'Please enter the amount of the transference:'
+                    },
+
+                ])
+                .then(answer => {
+
+                    const amount = answer['transferenceValue']
+                    
+                    exchangeValues(accountFrom, accountTo, parseFloat(amount))
+
+                })
+                .catch(err => { console.log(err) })
+
+            }
+
+        })
+        .catch(err => { console.log(err) })
+
+}
+
+function exchangeValues(accountFrom, accountTo, amount) {
+
+    const accountOrigin = getAccount(accountFrom)
+    const accountDestination = getAccount(accountTo)
+
+    if(!amount||amount < 0) {
+
+        console.log(chalk.bgRed.black('Desired amount is 0 or negative!'))
+        return transfer()
+
+    } else if (accountOrigin.balance < amount) {
+
+        console.log(chalk.bgRed.black('Balance unavailable.'))
+        return transfer()
+
+    } else {
+        
+        accountOrigin.balance -= amount
+        accountDestination.balance += amount
+        
+        fs.writeFileSync(`accounts/${accountFrom}.json`, JSON.stringify(accountOrigin), err => {console.log(err)})
+        fs.writeFileSync(`accounts/${accountTo}.json`, JSON.stringify(accountDestination), err => {console.log(err)})
+        
+        console.log(chalk.green.bold(`Transfer of U$${amount} successfully made between account ${accountFrom} and account ${accountTo}`))
+
+        operation()
+
+    }
 
 }
